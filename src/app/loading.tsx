35025548a98926161996dab1fb1971a49da7302f
erolmahmutoglu <erolmahmutoglu@outlook.com>
2024-03-04 22:24:08 +0300

    Add Loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,134 @@
+"use client";
+
+const LoadingPage = () => {
+  return (
+    <main className="md:container mx-auto flex min-h-screen flex-col items-center justify-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        style={{
+          margin: "auto",
+          background: "none",
+          display: "block",
+          shapeRendering: "auto",
+        }}
+        width="200px"
+        height="200px"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid"
+      >
+        <circle cx="84" cy="50" r="10" fill="#22c55e">
+          <animate
+            attributeName="r"
+            repeatCount="indefinite"
+            dur="0.25s"
+            calcMode="spline"
+            keyTimes="0;1"
+            values="10;0"
+            keySplines="0 0.5 0.5 1"
+            begin="0s"
+          ></animate>
+          <animate
+            attributeName="fill"
+            repeatCount="indefinite"
+            dur="1s"
+            calcMode="discrete"
+            keyTimes="0;0.25;0.5;0.75;1"
+            values="#22c55e;#facc15;#22c55e;#facc15;#22c55e"
+            begin="0s"
+          ></animate>
+        </circle>
+        <circle cx="16" cy="50" r="10" fill="#22c55e">
+          <animate
+            attributeName="r"
+            repeatCount="indefinite"
+            dur="1s"
+            calcMode="spline"
+            keyTimes="0;0.25;0.5;0.75;1"
+            values="0;0;10;10;10"
+            keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+            begin="0s"
+          ></animate>
+          <animate
+            attributeName="cx"
+            repeatCount="indefinite"
+            dur="1s"
+            calcMode="spline"
+            keyTimes="0;0.25;0.5;0.75;1"
+            values="16;16;16;50;84"
+            keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+            begin="0s"
+          ></animate>
+        </circle>
+        <circle cx="50" cy="50" r="10" fill="#facc15">
+          <animate
+            attributeName="r"
+            repeatCount="indefinite"
+            dur="1s"
+            calcMode="spline"
+            keyTimes="0;0.25;0.5;0.75;1"
+            values="0;0;10;10;10"
+            keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+            begin="-0.25s"
+          ></animate>
+          <animate
+            attributeName="cx"
+            repeatCount="indefinite"
+            dur="1s"
+            calcMode="spline"
+            keyTimes="0;0.25;0.5;0.75;1"
+            values="16;16;16;50;84"
+            keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+            begin="-0.25s"
+          ></animate>
+        </circle>
+        <circle cx="84" cy="50" r="10" fill="#22c55e">
+          <animate
+            attributeName="r"
+            repeatCount="indefinite"
+            dur="1s"
+            calcMode="spline"
+            keyTimes="0;0.25;0.5;0.75;1"
+            values="0;0;10;10;10"
+            keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+            begin="-0.5s"
+          ></animate>
+          <animate
+            attributeName="cx"
+            repeatCount="indefinite"
+            dur="1s"
+            calcMode="spline"
+            keyTimes="0;0.25;0.5;0.75;1"
+            values="16;16;16;50;84"
+            keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+            begin="-0.5s"
+          ></animate>
+        </circle>
+        <circle cx="16" cy="50" r="10" fill="#facc15">
+          <animate
+            attributeName="r"
+            repeatCount="indefinite"
+            dur="1s"
+            calcMode="spline"
+            keyTimes="0;0.25;0.5;0.75;1"
+            values="0;0;10;10;10"
+            keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+            begin="-0.75s"
+          ></animate>
+          <animate
+            attributeName="cx"
+            repeatCount="indefinite"
+            dur="1s"
+            calcMode="spline"
+            keyTimes="0;0.25;0.5;0.75;1"
+            values="16;16;16;50;84"
+            keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+            begin="-0.75s"
+          ></animate>
+        </circle>
+      </svg>
+    </main>
+  );
+};
+
+export default LoadingPage;
